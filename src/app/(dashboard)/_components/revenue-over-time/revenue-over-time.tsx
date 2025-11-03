c23011d5200428e84b52307b04af7661dc9b3e8a
hcampos <hcampospro@gmail.com>
2025-11-03 23:49:06 +0100

    chore(package.json): update dependencies to latest versions for improved stability and features fix(revenue-over-time.tsx): add type annotation to tickFormatter function for better type safety

--- src/app/(dashboard)/_components/revenue-over-time/revenue-over-time.tsx
+++ src/app/(dashboard)/_components/revenue-over-time/revenue-over-time.tsx
@@ -65,7 +65,7 @@ export const RevenueOverTime: Component = () => {
               tickLine={false}
               axisLine={false}
               tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}
+              tickFormatter={(value: string) => value.slice(0, 3)}
             />
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
             <defs>
